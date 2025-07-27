02098503f303d696ed75e75534e00b695d375429
user <donrose0088@gmail.com>
2025-07-27 20:25:56 +0800

    yr

--- decorator.py
+++ decorator.py
@@ -1,18 +1,25 @@
+#added function
 def add_sprinkles (func):
+    #must add the args/kwarggs to accept any number of arguments or keyword (Added in the base is the "flavor")
     def wrapper(*args, **kwarggs):
         print("*You add sprinkles 🎆*")
         func(*args, **kwarggs)
     return(wrapper)
 
+#added function
 def add_fudge(func):
     def wrapper(*args, **kwarggs):
         print("*You add fudge 🍫*")
         func(*args, **kwarggs)
     return (wrapper)
 
+#to apply, better use "@" sign for the remaining added function that is modfying the base 
 @add_sprinkles
 @add_fudge
+
+#base function
 def get_ice_cream(flavor):
+    #flavor is the added keyword argument
     print(f"Here is your {flavor} ice cream 🍦")
 
 get_ice_cream("vanilla")
